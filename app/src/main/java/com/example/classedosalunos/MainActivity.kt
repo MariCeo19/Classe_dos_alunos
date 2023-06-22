72202847fbe80000da2f94c51c156d3f3e22234c
Aluno <marianathayza3@gmail.com>
2023-06-22 17:26:26 -0300

    22/06/2023

--- app/src/main/java/com/example/classedosalunos/MainActivity.kt
+++ app/src/main/java/com/example/classedosalunos/MainActivity.kt
@@ -1,20 +1,32 @@
 package com.example.classedosalunos
 
+import android.hardware.ConsumerIrManager.CarrierFrequencyRange
 import android.os.Bundle
+import android.text.Layout
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ImageBitmap
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.classedosalunos.ui.theme.ClasseDosAlunosTheme
 
 class MainActivity : ComponentActivity() {
@@ -23,15 +35,41 @@ class MainActivity : ComponentActivity() {
         setContent {
             ClasseDosAlunosTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                  Fases()
+                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background){
+
                 }
             }
         }
     }
 }
+
+
 @Preview
 @Composable
-fun Fases() {
+fun Cara() {
+   Card() {}
+    Row() {
+        Column() {
+            Image(
+                painter = painterResource(id = R.drawable.harry_potter),
+                contentDescription = null,
+                contentScale = ContentScale.FillWidth,
+                modifier = Modifier
+                    .size(250.dp)
+                    .padding(10.dp)
+            )
+        }
+        Text(
+            text = "Nota:",
+            fontSize = 20.sp,
 
+
+        )
+        Text(
+            text = "Falta:",
+            fontSize = 20.sp,
+            modifier = Modifier
+                .padding()
+            )
+    }
 }
